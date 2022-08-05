19f6ed5c500ad366dd9876452c095aa528aa5083
Trevor Baker <trevor@trevorbphoto.com>
2022-08-05 11:12:15 -0700

    fixed frame flipping issue in update function

--- main.c
+++ main.c
@@ -19,7 +19,7 @@ int main() {
     int rule_key[8][3] = {{1,1,1},{1,1,0},{1,0,1},{1,0,0},{0,1,1},{0,1,0},{0,0,1},{0,0,0}};
 
     int FRAME_TIME = 100000;
-    int BOARD_SIZE = 83;
+    int BOARD_SIZE = 80;
     int *board = (int *)malloc(sizeof(int) * BOARD_SIZE);
     int *temp_board = (int *)malloc(sizeof(int) * BOARD_SIZE);
     
@@ -29,23 +29,27 @@ int main() {
 
     //fillRule test - 102
     struct Rule rule;
-    rule.i = 226;
+    rule.i = 73;
 
     rule = fillRule(rule);
     
     board = make_one_dot_board(board, BOARD_SIZE);
-    for (int i = 0; i < 100000; i++) {
+
+    for (int i = 0; i < 10000; i++) {
         print_board(board, BOARD_SIZE);
         board = update_board(board, temp_board, BOARD_SIZE, rule);
         usleep(FRAME_TIME);
     }
+    free(board);
+    free(temp_board);
+    return 0;
 }
 
 //functions
 struct Rule fillRule(struct Rule rule) {
     int x = rule.i;
     
-    for (int i = 7; i > -1; i--) {
+    for (int i = 0; i < 8; i++) {
         rule.b[i] = x % 2;
         x /= 2;
     }
@@ -84,7 +88,10 @@ int *update_board(int *board, int *new_board, int size, struct Rule rule) {
 
         new_board[i] = rule.b[bin];
     }
-    return new_board;
+    for (int i = 0; i < size; i++) {
+        board[i] = new_board[i];
+    }
+    return board;
 }
 
 void print_board(int *board, int size) {
